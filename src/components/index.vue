eba604c3b2d605a2130f7765d79dc713f075e49d
sunzhaoqkd <sunzhaoqkd@163.com>
2019-11-01 15:39:51 +0800

    update

--- src/components/index.vue
+++ src/components/index.vue
@@ -1061,14 +1061,38 @@ export default {
 		async getMemberDetail(){
 			await this.axios.post('/vc/member/getMemberDetail').then(res=>{
                 if(res.data.status==1){
-                    this.saveCookie(this.$route.query.customerId)
+					console.log(this.getCookie('rememberMatch'));
+					this.saveCookie(this.getCookie('rememberMatch'))
+					
 					this.saveUserInfo(res.data.data)
-					console.log(res.data.data)
+					// console.log(res.data.data)
                 }else{
                     console.log('暂未登陆！')
                     // this.$router.push('/');
                 }
             })
+		},
+		getCookie(cookie_name) {
+			var allcookies = document.cookie;
+			//索引长度，开始索引的位置
+			var cookie_pos = allcookies.indexOf(cookie_name);
+
+			// 如果找到了索引，就代表cookie存在,否则不存在
+			if (cookie_pos != -1) {
+				// 把cookie_pos放在值的开始，只要给值加1即可
+				//计算取cookie值得开始索引，加的1为“=”
+				cookie_pos = cookie_pos + cookie_name.length + 1; 
+				//计算取cookie值得结束索引
+				var cookie_end = allcookies.indexOf(";", cookie_pos);
+				
+				if (cookie_end == -1) {
+					cookie_end = allcookies.length;
+
+				}
+				//得到想要的cookie的值
+				var value = unescape(allcookies.substring(cookie_pos, cookie_end)); 
+			}
+			return value;
 		},
 		projectSizerClick(index){ // 我的项目访问记录
 			this.MyProjectsign = index;
--- src/components/login.vue
+++ src/components/login.vue
@@ -88,8 +88,9 @@ export default {
     computed: {
 		...mapState(['cookie','userInfo','bgColor'])
 	},
-    created(){
+    async created(){
         this.getloginCode();
+        await this.getMemberDetail()
     },
     mounted(){
         this.$nextTick(()=>{
@@ -97,6 +98,40 @@ export default {
         })
     },
     methods:{
+        async getMemberDetail(){
+			await this.axios.post('/vc/member/getMemberDetail').then(res=>{
+                if(res.data.status==1){
+                    this.saveCookie(this.getCookie('rememberMatch'))
+					this.saveUserInfo(res.data.data)
+					console.log(res.data.data)
+                }else{
+                    console.log('暂未登陆！')
+                    // this.$router.push('/');
+                }
+            })
+        },
+        getCookie(cookie_name) {
+			var allcookies = document.cookie;
+			//索引长度，开始索引的位置
+			var cookie_pos = allcookies.indexOf(cookie_name);
+
+			// 如果找到了索引，就代表cookie存在,否则不存在
+			if (cookie_pos != -1) {
+				// 把cookie_pos放在值的开始，只要给值加1即可
+				//计算取cookie值得开始索引，加的1为“=”
+				cookie_pos = cookie_pos + cookie_name.length + 1; 
+				//计算取cookie值得结束索引
+				var cookie_end = allcookies.indexOf(";", cookie_pos);
+				
+				if (cookie_end == -1) {
+					cookie_end = allcookies.length;
+
+				}
+				//得到想要的cookie的值
+				var value = unescape(allcookies.substring(cookie_pos, cookie_end)); 
+			}
+			return value;
+		},
         ...mapMutations(['saveCookie','saveUserInfo']),
         getloginCode(){ // 获取二维码
             this.axios.post('/vc/wxLog/wxLoginCode_BP').then(res=>{
